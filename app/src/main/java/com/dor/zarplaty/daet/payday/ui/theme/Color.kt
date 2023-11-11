d74226b0e9f7042909eddfc27e2dce234e2ab802
hayarovy <edurd@rambler.ru>
2023-11-11 07:52:06 +0500

    3

--- app/src/main/java/com/dor/zarplaty/daet/payday/ui/theme/Color.kt
+++ app/src/main/java/com/dor/zarplaty/daet/payday/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val baseBackground = Color(0xFFC9E4EB)
+val blue = Color(0xFF004A94)
+val white = Color(0xFFFFFFFF)
+val titleText = Color(0xFF090648)
+val secondText = Color(0xFF050434)
+val grey = Color(0x33282B39)
